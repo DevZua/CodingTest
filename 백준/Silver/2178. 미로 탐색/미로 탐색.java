d2ebddefeb44d0608dfc065c33c09f90086adffb
zua <154753143+DevZua@users.noreply.github.com>
2024-11-27 20:35:19 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,15 +1,17 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         // 입력 처리
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         int[][] maze = new int[N][M];
         for (int i = 0; i < N; i++) {
-            String line = sc.next();
+            String line = br.readLine();
             for (int j = 0; j < M; j++) {
                 maze[i][j] = line.charAt(j) - '0';
             }
